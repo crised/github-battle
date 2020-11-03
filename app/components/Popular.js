a8c444e5c8f29c94e7884c6e19947285ba410c87
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-11-03 18:59:03 -0300

    feat: Setting state, onClick listener
    Added onClick listener to component,
    function is setting the state with setState. Hence,
    the UI is being updated and the color of the button changes.
    
    footer

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage){
+    this.setState({
+      selectedLanguage: selectedLanguage
+    }) // Async
+  }
+
   render() {
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
     return (
       <ul className = 'flex-center'>
       {languages.map((language, index) => (
         <li key={index}>
-        <button className='btn-clear nav-link'>
+        <button
+          className='btn-clear nav-link'
+          style = {language === this.state.selectedLanguage ? { color: 'red'} : {color : 'black'}}
+          onClick={() =>  this.updateLanguage(language)}>
           {language}
         </button>
         </li>
