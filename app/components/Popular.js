1c43efa76588a985fac13e872958c6c5e8507a81
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-11-04 12:05:24 -0300

    feat: fetching from GitHub
    Added api.js that fetches from GitHub API.
    We added 2 variables to state: error, repos.
    If the fetch call fails, the error message will
    be bubbled to the UI code.
    
    footer

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
 
 function LanguagesNav({selected, onUpdateLanguage}){
   // console.log(props);
@@ -32,27 +33,56 @@ export default class Popular extends React.Component {
     super(props)
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
     }
 
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
 
   updateLanguage(selectedLanguage){
     this.setState({
-      selectedLanguage: selectedLanguage
+      selectedLanguage: selectedLanguage,
+      error: null,
+      repos: null
     }) // Async
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null
+      }))
+      .catch((error) => {
+        console.warn('Error fetching repos', error)
+
+        this.setState({
+        repos: null,
+        error: null
+      })})
+  }
+
+  isLoading(){
+    return this.state.repos === null && this.state.error === null
   }
 
   render() {
-    const { selectedLanguage } = this.state
+    const { selectedLanguage, repos, error } = this.state
 
     return(
       <React.Fragment>
         <LanguagesNav
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}/>
+
+      {this.isLoading() && <p>LOADING</p>}
+
+      {error && <p>{error}</p>}
+
+      {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
       </React.Fragment>
+
     )
 
   }
