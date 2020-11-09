613d5a06b70d164c755e0dbb158a64b2f65158c0
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-11-09 09:10:57 -0300

    feat: Stuffed the Instructions Component
    Added flesh to the Instructions component,
    by adding and Ordered list with 3 items containing
    headers and React Icons from FontAwesome.
    
    Also changed this component name to Battle in index.jss

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
-import Instructions from './components/Battle'
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-      <Instructions/>
+      <Battle/>
       <Popular/>
       </div>
     )
