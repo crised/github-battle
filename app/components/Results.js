ff46778524509de85b917238750837d2d76d0c3f
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-11-17 09:54:27 -0300

    feat: Added Card component, refactored Popular, Results
    Card component receives 5 string props: header,
    subheader (optional), avatar, href and name.
    In addition, the unordered list is passed as a
    React children.
    
    It displays a small div, with the following
    HTML elements: h4, img, optional h4,
    h2 and anchor. Plus, the ul (children)
    is appended.
    
    Results component is refactored to use 2 cards:
    one for the winner and one for the loser.
    
    Popular component is refactored to
    use the Card component.

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { battle } from '../utils/api'
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
-
+import Card from './Card'
 
 export default class Results extends React.Component {
 
@@ -36,7 +36,6 @@ export default class Results extends React.Component {
 
   render() {
     const { winner, loser, error, loading } = this.state
-    console.log(loser)
 
     if ( loading === true) {
       return <p> LOADING </p>
@@ -50,23 +49,13 @@ export default class Results extends React.Component {
     }
     return (
     <div className='grid space-around container-sm'>
-      <div className='card bg-light'>
-        <h4 className='header-lg center-text'>
-          {winner.score === loser.score ? 'Tie' : 'Winner' }
-        </h4>
-        <img
-          className='avatar'
-          src={winner.profile.avatar_url}
-          alt={`Avatar for ${winner.profile.login}`}
-        />
-        <h4 className='center-text'>
-          Score: {winner.score.toLocaleString()}
-        </h4>
-        <h2 className='center-text'>
-          <a className='link' href={winner.profile.html_url}>
-            {winner.profile.login}
-          </a>
-        </h2>
+      <Card
+        header={winner.score === loser.score ? 'Tie' : 'Winner' }
+        subheader={`Score: ${winner.score.toLocaleString()}`}
+        avatar={winner.profile.avatar_url}
+        href={winner.profile.html_url}
+        name={winner.profile.login}
+      >
         <ul className='card-list'>
           <li>
             <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -93,51 +82,41 @@ export default class Results extends React.Component {
               {winner.profile.following.toLocaleString()} following
           </li>
         </ul>
-      </div>
-      <div className='card bg-light'>
-      <h4 className='header-lg center-text'>
-          {winner.score === loser.score ? 'Tie' : 'Loser' }
-      </h4>
-      <img
-          className='avatar'
-          src={loser.profile.avatar_url}
-          alt={`Avatar for ${loser.profile.login}`}
-      />
-      <h4 className='center-text'>
-          Score: {loser.score.toLocaleString()}
-      </h4>
-      <h2 className='center-text'>
-        <a className='link' href={loser.profile.html_url}>
-          {loser.profile.login}
-        </a>
-      </h2>
-      <ul className='card-list'>
-        <li>
-          <FaUser color='rgb(239, 115, 115)' size={22} />
-          {loser.profile.name}
-        </li>
-        {loser.profile.location && (
+      </Card>
+      <Card
+        header={winner.score === loser.score ? 'Tie' : 'Loser' }
+        subheader={`Score: ${loser.score.toLocaleString()}`}
+        avatar={loser.profile.avatar_url}
+        href={loser.profile.html_url}
+        name={loser.profile.login}
+      >
+        <ul className='card-list'>
           <li>
-            <FaCompass color='rgb(144, 115, 255)' size={22} />
-            {loser.profile.location}
+            <FaUser color='rgb(239, 115, 115)' size={22} />
+            {loser.profile.name}
           </li>
-        )}
-        {loser.profile.company && (
+          {loser.profile.location && (
+            <li>
+              <FaCompass color='rgb(144, 115, 255)' size={22} />
+              {loser.profile.location}
+            </li>
+          )}
+          {loser.profile.company && (
+            <li>
+              <FaBriefcase color='#795548' size={22} />
+              {loser.profile.company}
+            </li>
+          )}
           <li>
-            <FaBriefcase color='#795548' size={22} />
-            {loser.profile.company}
+              <FaUsers color='rgb(129, 195, 245)' size={22} />
+              {loser.profile.followers.toLocaleString()} followers
           </li>
-        )}
-        <li>
-            <FaUsers color='rgb(129, 195, 245)' size={22} />
-            {loser.profile.followers.toLocaleString()} followers
-        </li>
-        <li>
-            <FaUsers color='rgb(64, 183, 95)' size={22} />
-            {loser.profile.following.toLocaleString()} following
-        </li>
-      </ul>
-      </div>
+          <li>
+              <FaUsers color='rgb(64, 183, 95)' size={22} />
+              {loser.profile.following.toLocaleString()} following
+          </li>
+        </ul>
+      </Card>
     </div>
     )
   }
